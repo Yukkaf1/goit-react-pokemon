745e0a1b7f820229eeb81d496e3fdbb529e0f370
Trunova Yulia <103053349+Yukkaf1@users.noreply.github.com>
2023-04-10 10:01:14 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'components/GlobalStyle';
+import { Example1 } from 'components/Example1';
+import { Reader } from 'components/Reader/Reader';
+import './index.css';
+import publications from './publications.json';
+
+const theme = {};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      {/* <Reader items={publications} /> */}
+      <Example1 />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
